a28b326a593d759ff9a178e37118f754e78ff0dc
Renuka Fernando <renukapiyumal@gmail.com>
2023-02-25 17:50:10 +0530

    Calculate results and display them in stdin
    Signed-off-by: Renuka Fernando <renukapiyumal@gmail.com>

--- jmeter/run-test.sh
+++ jmeter/run-test.sh
@@ -4,9 +4,12 @@ root_dir="${script_dir}/.."
 # Change directory to make sure logs directory is created inside $script_dir
 cd $script_dir
 
+test_namet="concurrancy-1"
 user_count=10
-heap_size="1g"
-duration=1200
+heap_size="500m"
+# heap_size="1g"
+duration=3
+# duration=1200
 payload_size=50B
 host=3.141.180.55
 server_ips="192.168.11.64"
@@ -14,7 +17,7 @@ server_ips="192.168.11.64"
 function usage() {
     echo ""
     echo "Usage: "
-    echo "$0 [-u <user_count>] [-s <payload_size>]"
+    echo "$0 [-n test_namet>] [-u <user_count>] [-s <payload_size>]"
     echo ""
     echo "-u: User Count."
     echo "-s: The Payload Size."
@@ -59,11 +62,11 @@ kubectl apply -f choreo-connect/ # -f netty-backend/
 sleep 120
 kubectl get po
 
-results_dir="${root_dir}/results/passthrough/${heap_size}_heap/${user_count}_users/${payload_size}/0ms_sleep"
+results_dir="${root_dir}/results/${test_namet}/passthrough/${heap_size}_heap/${user_count}_users/${payload_size}/0ms_sleep"
 echo "Results Dir: ${results_dir}"
 mkdir -p ${results_dir}
 kubectl top po --containers > "${results_dir}/start-resources.txt"
-nohup sh -c "sleep 30 && kubectl top po --containers > ${results_dir}/10min-resources.txt" > /dev/null &
+nohup sh -c "sleep 30 && kubectl top po --containers > ${results_dir}/middle-resources.txt" > /dev/null &
 
 # Client
 ssh cc-perf-test-client-1 \
@@ -79,7 +82,7 @@ echo "Paylod: ${payload_size}"
 
 export HEAP="-Xms${heap_size} -Xmx${heap_size}"
 
-results_dir="${HOME}/results/passthrough/${heap_size}_heap/${user_count}_users/${payload_size}/0ms_sleep"
+results_dir="${HOME}/results/${test_namet}/passthrough/${heap_size}_heap/${user_count}_users/${payload_size}/0ms_sleep"
 echo "Results Dir: ${results_dir}"
 
 user_count_per_server=$(($user_count / 2))
@@ -100,6 +103,35 @@ cd ./apache-jmeter-5.5/bin
     -Gtokens=${HOME}/jwt-tokens.csv \
     -l "${results_dir}/results.jtl" \
     -R "${server_ips}"
+
+cd "$results_dir"
+java -jar ~/jtl-splitter-0.4.6-SNAPSHOT.jar -f results.jtl -p -s -u SECONDS -t 3
+# java -jar ~/jtl-splitter-0.4.6-SNAPSHOT.jar -f results.jtl -p -s -u MINUTES -t 5
+
+tar -czf results.jtl.gz results.jtl
+rm results.jtl
+rm results-warmup.jtl
+rm results-warmup-summary.json
+rm results-measurement.jtl
+
+echo ""
+echo ""
+echo "Results"
+echo ""
+cat results-measurement-summary.json
 ENDSSH
 
 kubectl top po --containers > "${results_dir}/end-resources.txt"
+
+echo ""
+echo ""
+echo "Resources"
+echo ""
+echo "Start"
+cat "${results_dir}/start-resources.txt"
+echo ""
+echo "10 min"
+cat "${results_dir}/middle-resources.txt"
+echo ""
+echo "End"
+cat "${results_dir}/end-resources.txt"
