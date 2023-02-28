600b05a3d857519cc58fbb4b4dbab90f694eae10
Renuka Fernando <renukapiyumal@gmail.com>
2023-02-28 19:06:25 +0530

    Restart ingress when running tests
    Signed-off-by: Renuka Fernando <renukapiyumal@gmail.com>

--- jmeter/redeploy-cc.sh
+++ jmeter/redeploy-cc.sh
@@ -6,14 +6,19 @@ root_dir="${script_dir}/.."
 # Create deployment
 cd "${root_dir}/k8s-artifacts"
 kubectl delete -f choreo-connect/ -f netty-backend/ > /dev/null || true
+kubectl delete po -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx
+
+echo "Waiting 30s to cool down..."
+sleep 30
 kubectl apply -f choreo-connect/ -f netty-backend/ > /dev/null
 
 echo ""
 echo "Waiting for Deployments..."
+kubectl wait -n ingress-nginx --for condition=available --all --timeout 120s deploy
 kubectl wait --for condition=available --all --timeout 120s deploy
 echo ""
 kubectl get po
 echo ""
 
-echo "Waiting 30s to cool down..."
-sleep 30
+echo "Waiting 60s to cool down..."
+sleep 60
--- k8s-artifacts/install-ingress.sh
+++ k8s-artifacts/install-ingress.sh
@@ -1,1 +1,7 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/aws/deploy.yaml
+
+## Ingress used for CC 1.1.0-Beta
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/aws/deploy.yaml
+
+## Ingress used for CC 1.0.0-Beta
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/aws/deploy.yaml
